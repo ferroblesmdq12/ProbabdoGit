a55953ccb13ba5646f9b857049b47dd5b2dfa618
ferroblesmdq12 <fernandomdq12@gmail.com>
2023-07-29 14:09:55 -0300

    Principio del Proyecto

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/**< viendo como funciona todo */
+    color(228);
+
+    printf("\n\t\tProbando Sistema de Controlador de Versiones atravez de GIT!\n");
+    return 0;
+}
