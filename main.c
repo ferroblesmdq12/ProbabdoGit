345d7d38bf601750c4335cacc67cb962ef0a66f1
ferroblesmdq12 <fernandomdq12@gmail.com>
2023-07-29 14:35:40 -0300

    Se agrega otro color

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <conio.h>
 
 int main()
 {
@@ -7,11 +9,19 @@ int main()
     color(228);
 
     printf("\n\t\tProbando Sistema de Controlador de Versiones atravez de GIT!\n");
+    system("pause");
+
+      // MENU //
+    system("cls");
+    color(2599);
+    printf("OTRO COLOR");
+
+
     return 0;
 
-    // MENU //
 
-    system(pause);
+
+
 
 
 
